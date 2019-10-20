27e58db88da3373a47efc7dfdbbcabc047abd9f3
FredrickBarrett <f.barrett12@gmail.com>
2019-10-20 16:21:12 -0400

    added the orbs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // just to edit 
+let userDiv = document.querySelector(".user")
+console.log(userDiv)
+
+pageLoad()
+
+function pageLoad(){
+    makeOrb()
+}
+
+function makeOrb(){
+    const stage = document.createElement("section")
+    stage.classList.add("stage")
+    
+    const spanOrb = document.createElement("span")
+    spanOrb.classList.add("shadow")
+    
+    const orb = document.createElement("figure")
+    orb.classList.add("orb")
+    orb.append(spanOrb)
+    
+    stage.append(orb)
+    userDiv.append(stage)
+    
+}
