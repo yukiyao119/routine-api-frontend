ada2f100d715a515fd99335338c151ec813db015
yukiyao119 <ukiyao@hotmail.com>
2019-10-22 18:11:21 -0400

    new routine feature & RoutineAdaptor

--- src/RoutineAdaptor.js
+++ src/RoutineAdaptor.js
@@ -1,2 +1,35 @@
-// const url = file:///Users/yaoyuki/Development/group-projects/routine-api-frontend/index.html
+// const URL = http://localhost:3000/login
 
+class RoutineAdaptor {
+    
+    static getAllRoutines(username){
+        return  fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: username
+            })
+        }).then(res => res.json())
+    }
+
+    static createARoutine(newCategory, newTitle, userId){
+        return  fetch("http://localhost:3000/routines", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                category: newCategory,
+                title: newTitle,
+                user_id: userId
+            })
+        }).then(res => res.json())
+      }
+
+    
+
+}
